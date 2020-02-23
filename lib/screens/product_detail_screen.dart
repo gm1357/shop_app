d7e415e10beb0f289b12f5bccdcfaa203df720e1
Gabriel Machado <g_machado1@hotmail.com>
2020-02-23 19:42:27 -0300

    Implementado padrão de provider para pegar produtos

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static String routeName = 'product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
